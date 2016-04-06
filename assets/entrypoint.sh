e0f15ba39da50069bdcf14e7c4005a6f6acebd3d
Mark Riedesel <mark@klowner.com>
2016-04-06 14:20:26 -0500

    Add xe-linux-distribution

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
+XE_LINUX_DISTRIBUTION=/usr/sbin/xe-linux-distribution
+XE_LINUX_DISTRIBUTION_CACHE=/var/cache/xe-linux-distribution
+XE_DAEMON=/usr/sbin/xe-daemon
 
-
-# copy udev rules to host
+# copy udev rules to host (doesn't seem to work currently)
 for rule in /etc/udev/rules.d/*; do
 	cp ${rule} /host/etc/udev/rules.d/${rule}
 done
+
+# detect OS type
+${XE_LINUX_DISTRIBUTION} ${XE_LINUX_DISTRIBUTION_CACHE}
 
 # ensure that xenfs is mounted to /proc/xen
 if [[ ! -e /proc/xen/xenbus ]]; then
@@ -13,4 +18,4 @@ if [[ ! -e /proc/xen/xenbus ]]; then
 fi
 
 # start the daemon
-exec /usr/sbin/xe-daemon
+exec ${XE_DAEMON}
